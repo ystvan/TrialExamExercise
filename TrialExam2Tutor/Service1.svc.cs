7628c43a5acabbdd1f24d4a1f88ce258e0c036fd
ystvan <istvan.marki@gmail.com>
2016-11-10 15:35:24 +0100

    service methods
    working with DB

--- TrialExam2Tutor/Service1.svc.cs
+++ TrialExam2Tutor/Service1.svc.cs
@@ -10,6 +10,29 @@ namespace TrialExam2Tutor
 {
     public class SensorService : ISensorService
     {
-        
+        private SensorModel db = new SensorModel();
+
+        public void StoreData(SensorData data)
+        {
+            db.SensorDatas.Add(data);
+            db.SaveChanges();
+        }
+
+        public List<SensorData> GetAllDatas()
+        {
+            return db.SensorDatas.ToList();
+        }
+
+        public List<SensorData> GetDataFrom(string FromTimestamp)
+        {
+            return db.SensorDatas.Where(s => s.TimeStamp.CompareTo(FromTimestamp) > 0).ToList();
+        }
+
+        public List<SensorData> GetDataFromTo(string FromTimestamp, string ToTimestamp)
+        {
+            return
+                db.SensorDatas.Where(
+                    s => s.TimeStamp.CompareTo(FromTimestamp) > 0 && s.TimeStamp.CompareTo(ToTimestamp) < 0).ToList();
+        }
     }
 }
